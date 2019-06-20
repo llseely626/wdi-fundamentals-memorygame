e8c8507e1ed4647409e9465f6d0969e450bde0a2
Laura Seely <llseely626@gmail.com>
2019-06-19 21:59:52 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,14 +3,20 @@ console.log("Up and running!");
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen")
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}
+
+function checkforMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
+
+function flipCard(cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkforMatch();
+};
+flipCard (0);
+flipCard (2);
+
