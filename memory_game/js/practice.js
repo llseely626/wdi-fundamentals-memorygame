fbec061d38d98787f9a44c0dd4679ef1875b4dba
Laura Seely <lauraseely@Billys-Air.home>
2019-06-19 21:05:48 -0400

    created game logic

---
+++ memory_game/js/practice.js
@@ -0,0 +1,10 @@
+var grade = 86;
+if (grade >= 90) {
+	console.log("Student received an A.")
+} else if (grade >= 80) {
+	console.log("Student received a B.")
+} else if (grade >= 70) {
+	console.log("Student received a C.")
+} else {
+	console.log("Student failed :(");
+}
